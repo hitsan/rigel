649e9d994581894ebe9f8fbf0f4c35ee194b0515
hitsan <soledewa2828@gmail.com>
2021-10-23 03:38:08 +0900

    add CodeGen.h and test file

---
+++ include/rigel/CodeGen.h
@@ -0,0 +1,10 @@
+#ifndef CODEGEN_H
+#define CODEGEN_H
+#include "llvm/ADT/StringRef.h"
+#include "llvm/Support/raw_ostream.h"
+
+namespace rigel {
+
+};
+
+#endif
---
+++ test/testCodeGen.cpp
@@ -0,0 +1,3 @@
+#include <gtest/gtest.h>
+#include "../include/rigel/CodeGen.h"
+using namespace rigel;
