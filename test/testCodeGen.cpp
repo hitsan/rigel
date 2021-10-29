60e39ff7b28cc5c4865e577fe781f09be5293bbd
hitsan <soledewa2828@gmail.com>
2021-10-30 03:09:29 +0900

    add test

--- test/testCodeGen.cpp
+++ test/testCodeGen.cpp
@@ -1,10 +1,18 @@
 #include <gtest/gtest.h>
 #include "../include/rigel/CodeGen.h"
 #include "../include/rigel/Ast.h"
+#include "../include/rigel/Lexer.h"
+#include "../include/rigel/Parser.h"
 using namespace rigel;
 
 TEST(TestCodeGen, IntGen)
 {
+    llvm::StringRef st = R"(1)";
+    Lexer lx = Lexer(st);
+    Parser ps = Parser(lx);
+
+    auto exp = ps.parse();
+    exp->codegen();
     Expression* expression = new IntLiteral(1);
     expression->codeGen();
 }
