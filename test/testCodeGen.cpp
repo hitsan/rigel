2122b411f9473530aab065768ec73a89167d493a
root <hitsan>
2021-11-21 01:55:34 +0900

    modify testCode

--- test/testCodeGen.cpp
+++ test/testCodeGen.cpp
@@ -7,13 +7,13 @@ using namespace rigel;
 
 TEST(TestCodeGen, IntGen)
 {
-    llvm::StringRef st = "let a = 1";
-    Lexer lx = Lexer(st);
-    Parser ps = Parser(lx);
+    // test return 1
+    Identifier ident("a");
+    Expression* integer = new IntLiteral(1);
+    auto let = new LetStatement(ident, integer);
+ 
+    auto ret = codegen(let);
+    auto block = ret.getBlock();
 
-    auto exp = ps.parse();
-    exp->codegen();
-
-    std::string expection = R"(%1 = alloca i32, align 4
-                               store i32 1, i32* %1, align 4)";
+    std::string expection = R"(ret i32 1)";
 }
