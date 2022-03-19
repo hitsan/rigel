09c1639829e079baf6cbcc5ac443a624b0ec339f
hitsan <soledewa2828@gmail.com>
2022-03-19 16:53:55 +0900

    remove comment

--- test/testCodeGen.cpp
+++ test/testCodeGen.cpp
@@ -7,30 +7,8 @@
 #include "../include/rigel/Parser.h"
 using namespace rigel;
 
-// TEST(returnInt, input_custom_value)
-// {
-//     // ast
-//     Expression* twoLiteral = new IntLiteral(2);
-//     ReturnStatement* returnState = new ReturnStatement(twoLiteral);
-//     CodeGenerator* generator = new CodeGenerator();
-
-//     // TODO
-//     // It is wrong to pass the expression to the generator.
-//     generator->codeGen(returnState);
-
-//     struct stat buffer;
-//     int exist = stat("./test_bin/test.bc", &buffer);
-//     ASSERT_EQ(0, exist);
-
-//     // 
-//     int result = std::system("lli test_bin/test.bc");
-//     result /= 256;
-//     ASSERT_EQ(2, result);
-// }
-
 TEST(returnInt, input_custom_value)
 {
-    // ast
     Expression* twoLiteral = new IntLiteral(8);
     Expression* returnState = new ReturnStatement(twoLiteral);
     
@@ -47,25 +25,27 @@ TEST(returnInt, input_custom_value)
     exist = stat("./test_bin/test.bc", &buffer);
     ASSERT_EQ(0, exist);
 
-    // 
     int result = std::system("lli test_bin/test.bc");
     result /= 256;
     ASSERT_EQ(8, result);
 }
 
-// TEST(binaryExpression, plus_int_value)
-// {
-//     // ast
-//     Expression* one = new IntLiteral(1);
-//     Expression* two = new IntLiteral(2);
-//     BinaryExpression* binaryExpression = new BinaryExpression(OP_PLUS, one, two);
+TEST(binaryExpression, plus_int_value)
+{
+    Expression* one = new IntLiteral(1);
+    Expression* two = new IntLiteral(2);
+    BinaryExpression* binaryExpression = new BinaryExpression(OP_PLUS, one, two);
 
-//     llvm::LLVMContext context;
-//     llvm::Module *llvmModule = new llvm::Module("Module", context);
-//     CodeGenerator* generator = new CodeGenerator(llvmModule);
-//     generator->codeGen(binaryExpression);
+    llvm::LLVMContext context;
+    llvm::Module *llvmModule = new llvm::Module("Module", context);
+    CodeGenerator* generator = new CodeGenerator(llvmModule);
 
-//     struct stat buffer;
-//     int exist = stat("./test_bin/test.bc", &buffer);
-//     ASSERT_EQ(0, exist);
-// }
+    struct stat buffer;
+    int exist = stat("./test_bin/test.bc", &buffer);
+    if(!exist) unlink("./test_bin/test.bc");
+
+    generator->codeGen(binaryExpression);
+
+    exist = stat("./test_bin/test.bc", &buffer);
+    ASSERT_EQ(0, exist);
+}
