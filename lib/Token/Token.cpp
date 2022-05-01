db77ccbee908daa70ac9b7bac5b89474beed730a
hitsan <soledewa2828@gmail.com>
2022-05-01 14:14:31 +0900

    add getTokenType

--- lib/Token/Token.cpp
+++ lib/Token/Token.cpp
@@ -6,4 +6,8 @@ bool Token::equalsTokenType(TokenType type)
 {
     return (type == this->type) ? true : false;
 }
+TokenType Token::getTokenType()
+{
+    return this->type;
+}
 std::string Token::getLiteral() { return literal.str(); }
