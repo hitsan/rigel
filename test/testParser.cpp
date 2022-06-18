40247d905b628a8ae29e4a6d37cc4af3a0800220
hitsan <soledewa2828@gmail.com>
2022-06-18 15:08:03 +0900

    add test

--- test/testParser.cpp
+++ test/testParser.cpp
@@ -6,6 +6,11 @@
 #include "include/rigel/Ast.h"
 using namespace rigel;
 
+void checkExpression(std::unique_ptr<Expression> expression, )
+{
+
+}
+
 TEST(ParseReturnState, single_integer)
 {
     llvm::StringRef code = "return 4";
@@ -14,7 +19,7 @@ TEST(ParseReturnState, single_integer)
     std::unique_ptr<Statement> state = parser.parse();
     ASSERT_EQ(StatementType::RET, state->getType());
 
-    Expression* expression = state->getExpression();
+    std::unique_ptr<Expression> expression = state->getExpression();
     ASSERT_EQ(NodeType::INT, expression->getType());
     IntLiteral* exp = llvm::dyn_cast<IntLiteral>(expression);
     ASSERT_EQ(4, exp->getValue());
@@ -27,17 +32,17 @@ TEST(TestBinary_expression, plus_expression)
     Parser parser = Parser(lexer);
     std::unique_ptr<Statement> state = parser.parse();
 
-    Expression* expression = state->getExpression();
+    std::unique_ptr<Expression> expression = state->getExpression();
     ASSERT_EQ(NodeType::PLUS, expression->getType());
 
     BinaryExpression* binExpr = llvm::dyn_cast<BinaryExpression>(expression);
 
-    Expression* expLHand = binExpr->getLHand();
+    std::unique_ptr<Expression> expLHand = binExpr->getLHand();
     ASSERT_EQ(NodeType::INT, expLHand->getType());
     IntLiteral* lInt = llvm::dyn_cast<IntLiteral>(expLHand);
     ASSERT_EQ(1, lInt->getValue());
 
-    Expression* expRHand = binExpr->getRHand();
+    std::unique_ptr<Expression> expRHand = binExpr->getRHand();
     ASSERT_EQ(NodeType::INT, expLHand->getType());
     IntLiteral* rInt = llvm::dyn_cast<IntLiteral>(expRHand);
     ASSERT_EQ(2, rInt->getValue());
@@ -50,17 +55,17 @@ TEST(TestBinary_expression, mul_expression)
     Parser parser = Parser(lexer);
     std::unique_ptr<Statement> state = parser.parse();
 
-    Expression* expression = state->getExpression();
+    std::unique_ptr<Expression> expression = state->getExpression();
     ASSERT_EQ(NodeType::MUL, expression->getType());
 
     BinaryExpression* binExpr = llvm::dyn_cast<BinaryExpression>(expression);
 
-    Expression* expLHand = binExpr->getLHand();
+    std::unique_ptr<Expression> expLHand = binExpr->getLHand();
     ASSERT_EQ(NodeType::INT, expLHand->getType());
     IntLiteral* lInt = llvm::dyn_cast<IntLiteral>(expLHand);
     ASSERT_EQ(4, lInt->getValue());
 
-    Expression* expRHand = binExpr->getRHand();
+    std::unique_ptr<Expression> expRHand = binExpr->getRHand();
     ASSERT_EQ(NodeType::INT, expLHand->getType());
     IntLiteral* rInt = llvm::dyn_cast<IntLiteral>(expRHand);
     ASSERT_EQ(5, rInt->getValue());
@@ -73,25 +78,25 @@ TEST(TestBinary_expression, plus_mul_expression)
     Parser parser = Parser(lexer);
     std::unique_ptr<Statement> state = parser.parse();
 
-    Expression* plusExpression = state->getExpression();
+    std::unique_ptr<Expression> plusExpression = state->getExpression();
     ASSERT_EQ(NodeType::PLUS, plusExpression->getType());
 
     BinaryExpression* plusBinExpression = llvm::dyn_cast<BinaryExpression>(plusExpression);
-    Expression* plusLHand = plusBinExpression->getLHand();
+    std::unique_ptr<Expression> plusLHand = plusBinExpression->getLHand();
     ASSERT_EQ(NodeType::INT, plusLHand->getType());
     IntLiteral* plusLInt = llvm::dyn_cast<IntLiteral>(plusLHand);
     ASSERT_EQ(7, plusLInt->getValue());
 
-    Expression* plusRHand = plusBinExpression->getRHand();
+    std::unique_ptr<Expression> plusRHand = plusBinExpression->getRHand();
     BinaryExpression* mulExpression = llvm::dyn_cast<BinaryExpression>(plusRHand);
     ASSERT_EQ(NodeType::MUL, mulExpression->getType());
 
-    Expression* mulLHand = mulExpression->getLHand();
+    std::unique_ptr<Expression> mulLHand = mulExpression->getLHand();
     ASSERT_EQ(NodeType::INT, mulLHand->getType());
     IntLiteral* mulLInt = llvm::dyn_cast<IntLiteral>(mulLHand);
     ASSERT_EQ(4, mulLInt->getValue());
 
-    Expression* mulRHand = mulExpression->getRHand();
+    std::unique_ptr<Expression> mulRHand = mulExpression->getRHand();
     ASSERT_EQ(NodeType::INT, mulRHand->getType());
     IntLiteral* mulRInt = llvm::dyn_cast<IntLiteral>(mulRHand);
     ASSERT_EQ(5, mulRInt->getValue());
@@ -104,25 +109,25 @@ TEST(TestBinary_expression, mul_plus_expression)
     Parser parser = Parser(lexer);
     std::unique_ptr<Statement> state = parser.parse();
 
-    Expression* plusExpression = state->getExpression();
+    std::unique_ptr<Expression> plusExpression = state->getExpression();
     ASSERT_EQ(NodeType::PLUS, plusExpression->getType());
 
     BinaryExpression* plusBinExpression = llvm::dyn_cast<BinaryExpression>(plusExpression);
-    Expression* plusBinRExpression = plusBinExpression->getRHand();
+    std::unique_ptr<Expression> plusBinRExpression = plusBinExpression->getRHand();
     ASSERT_EQ(NodeType::INT, plusBinRExpression->getType());
     IntLiteral* plusRInt = llvm::dyn_cast<IntLiteral>(plusBinRExpression);
     ASSERT_EQ(6, plusRInt->getValue());
 
-    Expression* plusBinLExpression = plusBinExpression->getLHand();
+    std::unique_ptr<Expression> plusBinLExpression = plusBinExpression->getLHand();
     BinaryExpression* mulBinExpression = llvm::dyn_cast<BinaryExpression>(plusBinLExpression);
     ASSERT_EQ(NodeType::MUL, mulBinExpression->getType());
 
-    Expression* mulLHand = mulBinExpression->getLHand();
+    std::unique_ptr<Expression> mulLHand = mulBinExpression->getLHand();
     ASSERT_EQ(NodeType::INT, mulLHand->getType());
     IntLiteral* mulLInt = llvm::dyn_cast<IntLiteral>(mulLHand);
     ASSERT_EQ(9, mulLInt->getValue());
 
-    Expression* mulRHand = mulBinExpression->getRHand();
+    std::unique_ptr<Expression> mulRHand = mulBinExpression->getRHand();
     ASSERT_EQ(NodeType::INT, mulRHand->getType());
     IntLiteral* mulRInt = llvm::dyn_cast<IntLiteral>(mulRHand);
     ASSERT_EQ(2, mulRInt->getValue());
